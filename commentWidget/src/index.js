134ff5a1a11fad99224ecf6a4c5cb84b733d810b
Summersby89 <66993944+Summersby89@users.noreply.github.com>
2022-12-09 09:21:14 +0300

    Delete commentWidget directory

--- commentWidget/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentList from './components/comment-list';
-
-
-ReactDOM.render( 
-	<CommentList/> ,
-	document.getElementById('root')
-);
